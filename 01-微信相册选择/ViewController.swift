6ad3719c68d3aa1193a8a1a8d8910d04908c5710
wangyansnow <13146597377@163.com>
2017-08-29 17:24:33 +0800

    Initial Commit

---
+++ 01-微信相册选择/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  01-微信相册选择
+//
+//  Created by 王俨 on 2017/8/29.
+//  Copyright © 2017年 https://github.com/wangyansnow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
