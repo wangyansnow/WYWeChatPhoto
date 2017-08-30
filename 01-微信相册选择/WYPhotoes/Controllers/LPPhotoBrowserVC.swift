99031b901b9384dfb3123e320b798087abaf926e
wangyansnow <13146597377@163.com>
2017-08-30 09:52:09 +0800

    增加图片选择VC

---
+++ 01-微信相册选择/WYPhotoes/Controllers/LPPhotoBrowserVC.swift
@@ -0,0 +1,35 @@
+//
+//  LPPhotoBrowserVC.swift
+//  01-微信相册选择
+//
+//  Created by 王俨 on 2017/8/30.
+//  Copyright © 2017年 https://github.com/wangyansnow. All rights reserved.
+//
+
+import UIKit
+
+class LPPhotoBrowserVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
